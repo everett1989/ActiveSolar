45f394ed3b45fd9f305683ff6b53cd8fbd34aad2
extremezoo <efranco@albany.edu>
2014-05-31 00:47:54 -0400

    ok

--- js/ie8-alert.js
+++ js/ie8-alert.js
@@ -2,4 +2,4 @@
 
  
 
-alert("This  website is incompatable with Internet Explorer 8 or less. Please use a later version or use Firefox or Chrome.");
+alert("This website is incompatable with Internet Explorer 8 or less. Please use a later version or use Firefox or Chrome.");
