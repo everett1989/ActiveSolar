251840526b7277d45dd8c8f69ca69543ee5668ac
extremezoo <efranco@albany.edu>
2014-06-01 16:24:46 -0400

    ok

--- config.rb
+++ config.rb
@@ -11,6 +11,8 @@ sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
 
+
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
--- site/js/ie8-alert.js
+++ site/js/ie8-alert.js
@@ -1,3 +1,1 @@
-
-
 alert("This website is incompatable with Internet Explorer 8 or less. Please use a later version or use Firefox or Chrome.");
