5f69472baacd118738e0ef1f9c09c3ca7a169ef5
extremezoo <efranco@albany.edu>
2014-06-10 16:38:53 -0400

    make adjustmest to site,
    adjusted form,
    adjusted banner,
    added temp google api

--- js/foundation-options.js
+++ js/foundation-options.js
@@ -3,7 +3,7 @@ $(document).foundation({
 	"magellan-expedition": {
 		active_class: 'active',
       threshold: -47, // pixels from the top of the expedition for it to become fixes
-      destination_threshold: 50, // pixels from the top of destination for it to be considered active
+      destination_threshold: 70, // pixels from the top of destination for it to be considered active
       throttle_delay: 30, // calculation throttling to increase framerate
       fixed_top: 35 // top distance in pixels assigend to the fixed element on scroll
   },
@@ -11,7 +11,15 @@ $(document).foundation({
   'topbar':{
 
   	scrolltop : false
+  },
+
+  'orbit':{
+    slide_number: false,
+    timer_speed: 4000,
+    pause_on_hover:false
   }
+
+
 
 
 });
