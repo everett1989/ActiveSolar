32738121610ffc393630c454538e8b35b85ff6ed
extremezoo <efranco@albany.edu>
2014-06-13 21:34:56 -0400

    Improved google maps

--- site/source/js/google-api.js
+++ site/source/js/google-api.js
@@ -16,20 +16,26 @@ function initialize() {
 
   var mapOptions = {
     center: endLocation,
-    zoom: 14
+    zoom: 13
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
 
 
-  var marker = new google.maps.Marker({
+  var solarMarker = new google.maps.Marker({
     position: endLocation,
     map: map,
     title: 'Active Solar'
   });
-  // var infowindow = new google.maps.InfoWindow({
-  //   content: 'Testing'
-  // });
+
+  var solarAddress = "<div><strong>Active Solar</strong><br><br>2189 Cook Road,<br> Galway, NY 12074, USA</div>"
+  var solarInfowindow = new google.maps.InfoWindow({
+    content: solarAddress
+  });
+
+  solarInfowindow.open(map,solarMarker);
+  
+
 
   // google.maps.event.addListener(marker, 'click', function() {
   //   infowindow.open(map,marker);
@@ -67,38 +73,9 @@ function initialize() {
     //alert(place.formatted_address);
     // calcRoute(place.geometry.location);
      calcRoute(place.formatted_address);
+     solarInfowindow.close();
+     solarMarker.setMap(null);
 
-    // If the place has a geometry, then present it on a map.
-    // if (place.geometry.viewport) {
-    //   map.fitBounds(place.geometry.viewport);
-    // } else {
-    //   map.setCenter(place.geometry.location);
-    //   map.setZoom(17);  // Why 17? Because it looks good.
-    // }
-
-
-
-    // marker.setIcon(/** @type {google.maps.Icon} */({
-    //   url: place.icon,
-    //   size: new google.maps.Size(71, 71),
-    //   origin: new google.maps.Point(0, 0),
-    //   anchor: new google.maps.Point(17, 34),
-    //   scaledSize: new google.maps.Size(35, 35)
-    // }));
-    // marker.setPosition(place.geometry.location);
-    // marker.setVisible(true);
-
-    // var address = '';
-    // if (place.address_components) {
-    //   address = [
-    //     (place.address_components[0] && place.address_components[0].short_name || ''),
-    //     (place.address_components[1] && place.address_components[1].short_name || ''),
-    //     (place.address_components[2] && place.address_components[2].short_name || '')
-    //   ].join(' ');
-    // }
-
-    // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    // infowindow.open(map, marker);
   });
 
 }
@@ -106,6 +83,8 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function calcRoute(customerBegin) {
+
+  
   // var start = document.getElementById('start').value;
   // var end = document.getElementById('end').value;
   var start = customerBegin; //'saratoga springs, ny';
