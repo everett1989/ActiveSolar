649f68e8313b0cf79b40f02d255def3273e6a72f
extremezoo <efranco@albany.edu>
2014-07-10 00:33:35 -0400

    Made Changes for Kathy
    Removed sunflares in contact us page
    fixed spelling error
    changed our process Chart photo titles and adjusted size

--- js/foundation-options.js
+++ js/foundation-options.js
@@ -1,14 +1,15 @@
 $(document).foundation({
 
-	"magellan-expedition": {
+	"magellan-expedition": { 
 		active_class: 'active',
       threshold: -47, // pixels from the top of the expedition for it to become fixes
       destination_threshold: 70, // pixels from the top of destination for it to be considered active
+      //destination_threshold: 10, // pixels from the top of destination for it to be considered active
       throttle_delay: 30, // calculation throttling to increase framerate
       fixed_top: 35 // top distance in pixels assigend to the fixed element on scroll
-    },
-
-    'topbar':{
+    }, 
+ 
+    'topbar':{ 
 
      scrolltop : false
    },
